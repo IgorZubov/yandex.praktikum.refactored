114fd5ac979278c39a041170a776ae5dc8f0f529
Igor Zubov <igor.zubov.a@gmail.com>
2026-03-10 16:40:15 +0200

    Minor adjustment

--- src/components/form/Form.ts
+++ src/components/form/Form.ts
@@ -1,11 +1,29 @@
 import Block from '../../framework/Block'
+import type { BlockOwnProps } from '../../framework/Block';
 
-export default class Form extends Block {
+interface FormProps extends BlockOwnProps {
+  buttonName?: string;
+}
+
+export default class Form extends Block<FormProps> {
   protected template = `
     <form>
       <input type="text" placeholder="Логин" ref="login">
       <input type="password" placeholder="Пароль" ref="password">
-      <button>Submit</button>
+      <button>{{buttonName}}</button>
     </form>
   `;
+
+  protected events = {
+    submit: (event: Event) => {
+      event.preventDefault();
+      console.log(this.refs);
+      console.log((this.refs.password as HTMLInputElement).value);
+    },
+  };
+
+  componentDidMount() {
+    console.log('Form component mounted');
+    setTimeout(() => this.setProps({ buttonName: "Клик через 3 секунды!" }), 3000);
+  }
 }
--- src/framework/Block.ts
+++ src/framework/Block.ts
@@ -1,39 +1,124 @@
 import Handlebars from 'handlebars';
-export default abstract class Block<Props extends object> {
+
+export interface BlockOwnProps {
+  __children?: Array<{
+    component: Block<object>;
+    embed(node: DocumentFragment): void;
+  }>;
+  __refs?: Record<string, Element>;
+}
+
+type EventListType = Partial<Record<keyof HTMLElementEventMap, (e: Event) => void>>;
+
+export default abstract class Block<Props extends BlockOwnProps = BlockOwnProps> {
   protected abstract template: string;
-
+  
   protected props = {} as Props;
-
+  
   private domElement: Element | null = null;
+  
+  protected children: Block<object>[] = [];
+  
+  protected refs: Record<string, Element> = {};
+  
+  protected events: EventListType = {};
 
   constructor(props: Props = {} as Props) {
     this.props = props;
-  }
-
-  private compile(): Element | null {
-    const html = Handlebars.compile(this.template)(this.props);
-    const templateElement = document.createElement("template");
-
-    templateElement.innerHTML = html;
-
-    return templateElement.content.firstElementChild;
   }
 
   public element(): Element | null {
     if (!this.domElement) {
       this.render();
     }
-
     return this.domElement;
   }
-  
-  protected render() {
-    const fragment = this.compile();
 
+  public setProps(props: Partial<Props>) {
+    let needUpdate = true;
+    Object.keys(props).forEach(newKey => {
+      if (Object.keys(this.props).find(key => key === newKey) 
+        && this.props[newKey as keyof Props] === props[newKey as keyof Props]) {
+        needUpdate = false;
+      }
+    });
+    if (needUpdate) {
+      this.props = { ...this.props, ...props, __children: [], __refs: {} } as Props;
+      this.render();
+    }
+  }
+
+  protected componentDidMount() {}
+
+  private mountComponent() {
+    this.attachListeners();
+    this.componentDidMount();
+  }
+
+  protected componentWillUnmount() {}
+
+  private unmountComponent() {
+    if (this.domElement) {
+      this.children.reverse().forEach(child => child.unmountComponent());
+
+      this.componentWillUnmount();
+      this.removeListeners();
+    }
+  }
+
+  private attachListeners() {
+    for (const eventName in this.events) {
+      const eventCallback = this.events[eventName as keyof EventListType];
+      if (typeof eventCallback == 'function' && this.domElement) {
+        this.domElement.addEventListener(eventName, eventCallback);
+      }
+    }
+  }
+
+  private removeListeners() {
+    for (const eventName in this.events) {
+      const eventCallback = this.events[eventName as keyof EventListType];
+      if (typeof eventCallback === 'function' && this.domElement) {
+        this.domElement.removeEventListener(eventName, eventCallback);
+      }
+    }
+  }
+
+  protected render() {
+    this.unmountComponent();
+    const fragment = this.compile();
     if (this.domElement && fragment) {
       this.domElement.replaceWith(fragment);
     }
-
     this.domElement = fragment;
+    this.mountComponent();
+  }
+
+  private compile(): Element | null {
+    const html = Handlebars.compile(this.template)(this.props);
+    const templateElement = document.createElement('template');
+    templateElement.innerHTML = html;
+    const fragment = templateElement.content;
+
+    if (this.props.__children) {
+      this.children = this.props.__children.map((child) => child.component);
+
+      this.props.__children.forEach((child) => {
+        child.embed(fragment);
+      });
+    }
+
+    const defaultRefs = this.props?.__refs ?? {};
+    this.refs = Array.from(fragment.querySelectorAll('[ref]')).reduce(
+      (list, element) => {
+        const key = element.getAttribute('ref') as string;
+        list[key] = element as HTMLElement;
+        element.removeAttribute('ref');
+        return list;
+      },
+      defaultRefs,
+    );
+
+    return templateElement.content.firstElementChild;
   }
 }
