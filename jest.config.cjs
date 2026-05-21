fe3a64d8a5681e1e37e8910adb1a297822b2181b
Igor Zubov <igor.zubov.a@gmail.com>
2026-05-21 06:54:45 +0300

    Add tests

---
+++ jest.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.ts$': ['ts-jest', { tsconfig: 'tsconfig.jest.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js'],
+};
